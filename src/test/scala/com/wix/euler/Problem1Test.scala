de4647a94e84afbd8bf8d2db57c1f377cd7bddba
Andrius Stanaitis <andriuss@wix.com>
2015-09-24 15:47:55 +0300

    Problem 1 and 2 solved

--- src/test/scala/com/wix/euler/Problem1Test.scala
+++ src/test/scala/com/wix/euler/Problem1Test.scala
@@ -12,11 +12,22 @@ The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-  def solve(till: Int): Int = ???
+  def solve(till: Int): Int = {
+    val range3 = Range(3, till, 3)
+    val range5 = Range(5, till, 5)
+    val set = range3.toSet ++ range5.toSet
+    set.sum
+
+    (1 until till).filter(x => x % 3 == 0 || x % 5 == 0).sum
+  }
 
   "solve" should {
     "return 23 for 1 to 10" in {
       solve(till = 10) must_== 23
+    }
+
+    "return xx for 1 to 1000" in {
+      solve(till = 1000) must_== 233168
     }
   }
 }
