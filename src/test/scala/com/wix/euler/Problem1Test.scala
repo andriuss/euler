bad68ed051fb282e9fe97f758f39af8314b611b6
Andrius Stanaitis <andriuss@wix.com>
2015-09-24 14:24:00 +0300

    Add Problem2

--- src/test/scala/com/wix/euler/Problem1Test.scala
+++ src/test/scala/com/wix/euler/Problem1Test.scala
@@ -5,7 +5,6 @@ import org.specs2.mutable.Specification
 class Problem1Test extends Specification {
 /*
 Multiples of 3 and 5
-
 Problem 1
 
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
